446800b7ff1042a33c1734ab387778d56de47dc8
shabari-123 <shabarimgr123@gmail.com>
2026-04-30 09:40:06 +0100

    First commit

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -1,10 +1,11 @@
 #Simple program to greet user
-def greet():
+def greetName():
     name=input("Enter your name: ")
+    #Empty name validation
     if name:
         print(f"Hello, {name}! Welcome to the program")
     else:
         print("Hello! you did not enter your name")
     
 #call the function
-greet()
+greetName()
